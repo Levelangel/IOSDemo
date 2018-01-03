f9f5e00a5698c7ff3e00c4fcdf58dfe1593ca01b
levelangel <levelangel@live.com>
2018-01-03 20:52:13 +0800

    Initial Commit

---
+++ ex1Tests/ex1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ex1Tests.swift
+//  ex1Tests
+//
+//  Created by Albert Chen on 2018/1/3.
+//  Copyright © 2018年 Albert Chen. All rights reserved.
+//
+
+import XCTest
+@testable import ex1
+
+class ex1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
