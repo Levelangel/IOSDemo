e9f4aa3631d889cc6ef05cf1111706100c2d4a4e
Albert Chen <lck@AlbertdeMac-mini.local>
2018-01-11 14:14:10 +0800

    完成翻页

--- ex1/ViewController.swift
+++ ex1/ViewController.swift
@@ -10,47 +10,140 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var isTurn : Bool = false
+    let MARGIN_LEFT_AND_RIGHT : CGFloat = 20.0
+    let MARGIN_TOP : CGFloat = 60.0
+    let MARGIN_BOTTOM : CGFloat = 30.0
     
-    @IBOutlet weak var viewContent: UIView!
-    @IBOutlet weak var btnNotKnow: UIButton!
-    @IBOutlet weak var btnNotShre: UIButton!
-    @IBOutlet weak var btnKnow: UIButton!
-    @IBOutlet weak var viewTitle: UIView!
+    let SCREEN_WIDTH = UIScreen.main.bounds.width
+    let SCREEN_HEIGHT = UIScreen.main.bounds.height
+    
+    let VIEW_CORNER_RADIUS : CGFloat = 10.0 //view圆角
+    
+    let CHILD_VIEW_BORDER_COLOR : UIColor = UIColor(red: 0xE0/255, green: 0xE5/255, blue: 0xED/255, alpha: 1.0)
+    let CHILD_VIEW_BACKGROUND_COLOR : UIColor = UIColor(red: 0xFF/255, green: 0xFF/255, blue: 0xFF/255, alpha: 1.0)
+    let TITLE_FONT_COLOR : UIColor = UIColor(red: 0x4D/255, green: 0x57/255, blue: 0x64/255, alpha: 1.0)
+    let BUTTON_TITLE_COLOR : UIColor = UIColor(red: 0xAF/255, green: 0xAD/255, blue: 0xAD/255, alpha: 1.0)
+    
+    var btn_height : CGFloat!
+    var btn_width : CGFloat!
+    
+    var viewContent : UIView!
+    var labelFront : UILabel!
+    var labelBack : UILabel!
+    var btnKnow : UIButton!
+    var btnNotKnow : UIButton!
+    var btnNotSure : UIButton!
+
+    var isFront : Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        viewTitle.layer.cornerRadius = 10.0
-        viewContent.layer.cornerRadius = 10.0
-        viewContent.isHidden = true
-    }
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for touch: AnyObject in touches {
-//            let t:UITouch = touch as! UITouch
-//            //当在屏幕上连续拍动两下时，背景恢复为白色
-//            if(t.tapCount == 2)
-//            {
-//                self.view.backgroundColor = UIColor.white
-//            }
-//                //当在屏幕上单击时，屏幕变为红色
-//            else if(t.tapCount == 1)
-//            {
-//                self.view.backgroundColor = UIColor.red
-//            }
-            
-//        }
-        if !isTurn {
-            UIView.beginAnimations("animation", context: nil)
-            UIView.setAnimationDuration(0.2)
-            UIView.setAnimationCurve(.linear)
-            UIView.setAnimationTransition(.flipFromLeft, for: viewTitle, cache: false)
-            UIView.commitAnimations()
-            isTurn = true
-            viewContent.isHidden = false
-        }
+        btn_width = (SCREEN_WIDTH-MARGIN_LEFT_AND_RIGHT*2)/3.0
+        btn_height = 80.0
+        
+        self.view.backgroundColor = UIColor(red: 0xF8/255, green: 0xF9/255, blue: 0xFA/255, alpha: 1.0)
+        
+        viewContent = initSubview()
+        labelFront = initFrontLabel()
+        labelBack = initBackLabel()
+        btnKnow = initBtnKnow()
+        btnNotKnow = initBtnNotKnow()
+        btnNotSure = initBtnNotSure()
+        viewContent.addSubview(labelFront)
+        self.view.addSubview(viewContent)
+        
+        
     }
     
+    func initSubview() -> UIView {
+        let subview = UIView(frame: CGRect(x: MARGIN_LEFT_AND_RIGHT, y: MARGIN_TOP, width: SCREEN_WIDTH-MARGIN_LEFT_AND_RIGHT*2-2, height: SCREEN_HEIGHT-MARGIN_TOP-MARGIN_BOTTOM-2))
+        subview.layer.borderColor = CHILD_VIEW_BORDER_COLOR.cgColor
+        subview.layer.borderWidth = 1.0
+        subview.layer.cornerRadius = VIEW_CORNER_RADIUS
+        subview.layer.backgroundColor = CHILD_VIEW_BACKGROUND_COLOR.cgColor
+        subview.layer.shadowColor = UIColor(red: 0xE6/255, green: 0xE6/255, blue: 0xE6/255, alpha: 1.0).cgColor
+        subview.layer.shadowOffset = CGSize(width:6,height:6)
+        subview.layer.shadowOpacity = 0.4
+        subview.isUserInteractionEnabled = true
+        subview.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tagGesture)))
+        return subview
+    }
+    
+    func initFrontLabel() -> UILabel {
+        let label : UILabel!
+        label = UILabel(frame: CGRect(x: MARGIN_LEFT_AND_RIGHT, y: 200, width: SCREEN_WIDTH-MARGIN_LEFT_AND_RIGHT*4, height: 100))
+        label.text = "汤姆米切尔提出的机器学习的定义"
+        label.font = UIFont.systemFont(ofSize: 26)
+        label.textColor = TITLE_FONT_COLOR
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }
+    
+    func initBackLabel() -> UILabel {
+        let label : UILabel!
+        label = UILabel(frame: CGRect(x: MARGIN_LEFT_AND_RIGHT, y: 60, width: SCREEN_WIDTH-MARGIN_LEFT_AND_RIGHT*4, height: 100))
+        label.text = "汤姆米切尔提出的机器学习的定义"
+        label.font = UIFont.systemFont(ofSize: 26)
+        label.textColor = TITLE_FONT_COLOR
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }
+    
+    func initBtnKnow() -> UIButton {
+        let button : UIButton!
+        button = UIButton(frame: CGRect(x: 0, y:viewContent.bounds.height-btn_height, width: btn_width, height: btn_height))
+        button.backgroundColor = UIColor(red: 0xF8/255, green: 0xF9/255, blue: 0xFA/255, alpha: 1.0)
+        button.setTitle("知道", for: .normal)
+        button.setTitleColor(BUTTON_TITLE_COLOR, for: .normal)
+        button.layer.borderWidth = 1.0
+        button.layer.borderColor = CHILD_VIEW_BORDER_COLOR.cgColor
+        
+        return button
+    }
+
+    func initBtnNotKnow() -> UIButton {
+        let button : UIButton!
+        button = UIButton(frame: CGRect(x: btn_width*2-2, y:viewContent.bounds.height-btn_height, width: btn_width, height: btn_height))
+        button.backgroundColor = UIColor(red: 0xF8/255, green: 0xF9/255, blue: 0xFA/255, alpha: 1.0)
+        button.setTitle("不知道", for: .normal)
+        button.setTitleColor(BUTTON_TITLE_COLOR, for: .normal)
+        button.layer.borderWidth = 1.0
+        button.layer.borderColor = CHILD_VIEW_BORDER_COLOR.cgColor
+        return button
+    }
+    
+    func initBtnNotSure() -> UIButton {
+        let button : UIButton!
+        button = UIButton(frame: CGRect(x: btn_width-1, y:viewContent.bounds.height-btn_height, width: btn_width, height: btn_height))
+        button.backgroundColor = UIColor(red: 0xF8/255, green: 0xF9/255, blue: 0xFA/255, alpha: 1.0)
+        button.setTitle("不确定", for: .normal)
+        button.setTitleColor(BUTTON_TITLE_COLOR, for: .normal)
+        button.layer.borderWidth = 1.0
+        button.layer.borderColor = CHILD_VIEW_BORDER_COLOR.cgColor
+        return button
+    }
+    
+    // View点击事件
+    @objc func tagGesture() {
+        UIView.beginAnimations("animation", context: nil)
+        UIView.setAnimationDuration(0.5)
+        UIView.setAnimationCurve(.easeOut)
+        UIView.setAnimationTransition(.flipFromLeft, for: viewContent, cache: false)
+        UIView.commitAnimations()
+        viewContent.subviews.map{ $0.removeFromSuperview()}
+        if isFront {
+            viewContent.addSubview(labelBack)
+            viewContent.addSubview(btnKnow)
+            viewContent.addSubview(btnNotKnow)
+            viewContent.addSubview(btnNotSure)
+        } else {
+            viewContent.addSubview(labelFront)
+        }
+        isFront = !isFront
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
