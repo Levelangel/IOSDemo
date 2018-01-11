f17e661bd92ca4bc85d1ec502f7790c7f0a2ab58
Albert Chen <lck@AlbertdeMac-mini.local>
2018-01-11 19:00:32 +0800

    修正优化翻转

--- ex1/ViewController.swift
+++ ex1/ViewController.swift
@@ -24,10 +24,14 @@ class ViewController: UIViewController {
     let TITLE_FONT_COLOR : UIColor = UIColor(red: 0x4D/255, green: 0x57/255, blue: 0x64/255, alpha: 1.0)
     let BUTTON_TITLE_COLOR : UIColor = UIColor(red: 0xAF/255, green: 0xAD/255, blue: 0xAD/255, alpha: 1.0)
     
+    let ANIMATION_DURATION : Double = 0.5
+    
     var btn_height : CGFloat!
     var btn_width : CGFloat!
     
     var viewContent : UIView!
+    var viewFront : UIView!
+    
     var labelFront : UILabel!
     var labelBack : UILabel!
     var btnKnow : UIButton!
@@ -35,6 +39,7 @@ class ViewController: UIViewController {
     var btnNotSure : UIButton!
 
     var isFront : Bool = true
+    var isLock : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,7 +64,7 @@ class ViewController: UIViewController {
     func initSubview() -> UIView {
         let subview = UIView(frame: CGRect(x: MARGIN_LEFT_AND_RIGHT, y: MARGIN_TOP, width: SCREEN_WIDTH-MARGIN_LEFT_AND_RIGHT*2-2, height: SCREEN_HEIGHT-MARGIN_TOP-MARGIN_BOTTOM-2))
         subview.layer.borderColor = CHILD_VIEW_BORDER_COLOR.cgColor
-        subview.layer.borderWidth = 1.0
+        subview.layer.borderWidth = 0.5
         subview.layer.cornerRadius = VIEW_CORNER_RADIUS
         subview.layer.backgroundColor = CHILD_VIEW_BACKGROUND_COLOR.cgColor
         subview.layer.shadowColor = UIColor(red: 0xE6/255, green: 0xE6/255, blue: 0xE6/255, alpha: 1.0).cgColor
@@ -67,6 +72,7 @@ class ViewController: UIViewController {
         subview.layer.shadowOpacity = 0.4
         subview.isUserInteractionEnabled = true
         subview.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tagGesture)))
+        subview.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(tagDrag)))
         return subview
     }
     
@@ -98,7 +104,7 @@ class ViewController: UIViewController {
         button.backgroundColor = UIColor(red: 0xF8/255, green: 0xF9/255, blue: 0xFA/255, alpha: 1.0)
         button.setTitle("知道", for: .normal)
         button.setTitleColor(BUTTON_TITLE_COLOR, for: .normal)
-        button.layer.borderWidth = 1.0
+        button.layer.borderWidth = 0.5
         button.layer.borderColor = CHILD_VIEW_BORDER_COLOR.cgColor
         
         return button
@@ -106,43 +112,67 @@ class ViewController: UIViewController {
 
     func initBtnNotKnow() -> UIButton {
         let button : UIButton!
-        button = UIButton(frame: CGRect(x: btn_width*2-2, y:viewContent.bounds.height-btn_height, width: btn_width, height: btn_height))
+        button = UIButton(frame: CGRect(x: btn_width*2-1, y:viewContent.bounds.height-btn_height, width: btn_width, height: btn_height))
         button.backgroundColor = UIColor(red: 0xF8/255, green: 0xF9/255, blue: 0xFA/255, alpha: 1.0)
         button.setTitle("不知道", for: .normal)
         button.setTitleColor(BUTTON_TITLE_COLOR, for: .normal)
-        button.layer.borderWidth = 1.0
+        button.layer.borderWidth = 0.5
         button.layer.borderColor = CHILD_VIEW_BORDER_COLOR.cgColor
         return button
     }
     
     func initBtnNotSure() -> UIButton {
         let button : UIButton!
-        button = UIButton(frame: CGRect(x: btn_width-1, y:viewContent.bounds.height-btn_height, width: btn_width, height: btn_height))
+        button = UIButton(frame: CGRect(x: btn_width-0.5, y:viewContent.bounds.height-btn_height, width: btn_width, height: btn_height))
         button.backgroundColor = UIColor(red: 0xF8/255, green: 0xF9/255, blue: 0xFA/255, alpha: 1.0)
         button.setTitle("不确定", for: .normal)
         button.setTitleColor(BUTTON_TITLE_COLOR, for: .normal)
-        button.layer.borderWidth = 1.0
+        button.layer.borderWidth = 0.5
         button.layer.borderColor = CHILD_VIEW_BORDER_COLOR.cgColor
         return button
     }
-    
+    var animationFront,animationBack : UIViewPropertyAnimator!
     // View点击事件
     @objc func tagGesture() {
-        UIView.beginAnimations("animation", context: nil)
-        UIView.setAnimationDuration(0.5)
-        UIView.setAnimationCurve(.easeOut)
-        UIView.setAnimationTransition(.flipFromLeft, for: viewContent, cache: false)
-        UIView.commitAnimations()
-        viewContent.subviews.map{ $0.removeFromSuperview()}
-        if isFront {
-            viewContent.addSubview(labelBack)
-            viewContent.addSubview(btnKnow)
-            viewContent.addSubview(btnNotKnow)
-            viewContent.addSubview(btnNotSure)
-        } else {
-            viewContent.addSubview(labelFront)
+
+        animationFront = UIViewPropertyAnimator(duration: 0.1, curve: .easeIn, animations: {
+            self.viewContent.layer.transform = CATransform3DMakeRotation(CGFloat(.pi*0.5), 0, -1, 0)
+        })
+        animationFront.startAnimation()
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) {_ in
+            self.viewContent.subviews.map{ $0.removeFromSuperview()}
+            if self.isFront {
+                self.viewContent.addSubview(self.labelBack)
+                self.viewContent.addSubview(self.btnKnow)
+                self.viewContent.addSubview(self.btnNotKnow)
+                self.viewContent.addSubview(self.btnNotSure)
+            } else {
+                self.viewContent.addSubview(self.labelFront)
+            }
+            self.isFront = !self.isFront
+            self.viewContent.layer.transform = CATransform3DMakeRotation(CGFloat(.pi*3.5), 0, -1, 0)
+            self.animationBack = UIViewPropertyAnimator(duration: 0.1, curve: .easeOut, animations: {
+                self.viewContent.layer.transform = CATransform3DMakeRotation(CGFloat(.pi*4.0), 0, -1, 0)
+            })
+            self.animationBack.startAnimation()
         }
-        isFront = !isFront
+        
+    }
+    
+    //View 拖拽事件
+    @objc func tagDrag(sender: UIPanGestureRecognizer) {
+        if isFront {
+            return
+        }
+        
+        let transition:CGPoint = sender.translation(in: viewContent)
+        checkLock(transition)
+    }
+    
+    func checkLock(_ transition : CGPoint){
+        if(abs(transition.x) > 1 || abs(transition.y) > 1){
+            isLock = true
+        }
     }
     
     override func didReceiveMemoryWarning() {
